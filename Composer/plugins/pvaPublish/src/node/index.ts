ec91ca9fbf4828704b4532dbfceb07ef543172cf
Tony Anziano <tonyanziano5@gmail.com>
2020-10-15 12:23:50 -0700

    Updated pvaPublish package config to new model

--- Composer/plugins/pvaPublish/src/node/index.ts
+++ Composer/plugins/pvaPublish/src/node/index.ts
@@ -3,7 +3,7 @@ import { getStatus, history, publish, pull } from './publish';
 function initialize(registration) {
   const plugin = {
     customDescription: 'Publish bot to Power Virtual Agents (Preview)',
-    hasView: true /** we have custom UI to host */,
+    bundleId: 'publish',
     history,
     getStatus,
     publish,
